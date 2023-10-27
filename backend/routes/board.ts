b960c1262df4d0b40685ecb948f15c586cd40c64
DongHyeonWon <dhe7700@naver.com>
2023-10-27 23:34:19 +0900

    fix: git ignore

--- backend/routes/board.ts
+++ backend/routes/board.ts
@@ -1,14 +1,11 @@
 import express from "express";
 import awsUpload from "../config/multerConfig";
-const path = require("path");
 const boardController = require("../controllers/board.ts");
 
 const router = express.Router();
 
 router.post("/", awsUpload.single("imgFile"), boardController.postAddBoard);
 
-router.get("/", (req, res, next) => {
-  console.log("get");
-});
+router.get("/", boardController.getBoard);
 
 module.exports = router;
