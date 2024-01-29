5f7692cbfeb1d8e1355c034942db652d28ef7538
DongHyeonWon <dhe7700@naver.com>
2024-01-29 15:53:42 +0900

    chore: 스크립트 및 config 변경

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,15 +4,18 @@ const nextConfig = {
     unoptimized: true,
     domains: ["hanroro-fanpage.s3.ap-northeast-2.amazonaws.com"],
   },
-  async rewrites() {
+  output: "export",
+  reactStrictMode: false,
+};
+if (process.env.NODE_ENV === "development") {
+  nextConfig.rewrites = async () => {
     return [
       {
         source: "/api/:path*",
         destination: process.env.NEXT_PUBLIC_BASE_URL + "/:path*",
       },
     ];
-  },
-  output: "export",
-  reactStrictMode: false,
-};
+  };
+}
+
 module.exports = nextConfig;
