4abf36d50c8c3e3391340daa6b9e81eefc037fd8
DongHyeonWon <dhe7700@naver.com>
2024-01-29 16:35:12 +0900

    chore: config 설정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,14 +4,15 @@ const nextConfig = {
     unoptimized: true,
     domains: ["hanroro-fanpage.s3.ap-northeast-2.amazonaws.com"],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: process.env.NEXT_PUBLIC_BASE_URL + "/:path*",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: process.env.NEXT_PUBLIC_BASE_URL + "/:path*",
+  //     },
+  //   ];
+  // },
+  output: "export",
   reactStrictMode: false,
 };
 module.exports = nextConfig;
