390009e45a44ce083c1546ae71f7a468fe3ba797
DongHyeonWon <dhe7700@naver.com>
2024-02-05 17:47:40 +0900

    reset: axios header

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const axiosApi = (url: string | undefined) =>
   axios.create({
     baseURL: url,
-    withCredentials: true,
   });
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
