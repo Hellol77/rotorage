db8e7578965fb9078c26437cab17ab860ade63e1
DongHyeonWon <dhe7700@naver.com>
2024-02-05 17:44:18 +0900

    chore: axios 설정

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -4,10 +4,6 @@ export const axiosApi = (url: string | undefined) =>
   axios.create({
     baseURL: url,
     withCredentials: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": "true",
-    },
   });
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
