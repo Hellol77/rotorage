02e94e3f662bd123c446194b94533b3eaece09c3
DongHyeonWon <dhe7700@naver.com>
2024-02-05 17:33:53 +0900

    reset: axios 설정

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -3,11 +3,6 @@ import axios from "axios";
 export const axiosApi = (url: string | undefined) =>
   axios.create({
     baseURL: url,
-    withCredentials: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": "true",
-    },
   });
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
