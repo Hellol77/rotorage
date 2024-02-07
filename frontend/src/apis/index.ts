89d00a8ce6d28956d2d6acf75b733218b19c1584
DongHyeonWon <dhe7700@naver.com>
2024-02-07 17:52:16 +0900

    fix: axios 설정

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export const axiosApi = (url: string | undefined) =>
   axios.create({
     baseURL: url,
+    withCredentials: true,
   });
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
