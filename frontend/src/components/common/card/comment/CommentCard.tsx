51a80f09a0dfa641042536cc4447ec86009dd822
DongHyeonWon <dhe7700@naver.com>
2024-05-05 22:17:44 +0900

    style: 댓글 작성자 폰트 굵기 수정

--- frontend/src/components/common/card/comment/CommentCard.tsx
+++ frontend/src/components/common/card/comment/CommentCard.tsx
@@ -21,13 +21,12 @@ export default function CommentCard({
     <ul className="flex w-full flex-shrink-0 pt-5">
       <ProfileAvatar size="small" profileImage={commentProfileImage} />
       <div className="ml-3 w-full">
-        <div className="w-fit whitespace-normal text-sm leading-4 text-gray-200 md:text-lg ">
+        <div className="w-fit whitespace-normal font-Pretendard-SemiBold text-xs leading-4 text-gray-200 md:text-lg ">
           {commentUserNickname}&nbsp;&nbsp;&nbsp;
           <span className="w-full whitespace-normal break-all font-Pretendard-Regular text-xs text-white  md:text-sm">
             {commentContent}
           </span>
         </div>
-
         <div className="mt-2 text-xs tracking-wide text-gray-400">
           {relativeDate(commentCreatedAt)}
         </div>
