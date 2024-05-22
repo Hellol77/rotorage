3416b2614115200777b17f820063d4f0922b82aa
DongHyeonWon <dhe7700@naver.com>
2024-05-22 18:26:15 +0900

    feat: 이미지 교체

--- frontend/src/components/common/section/CircleImageSection.tsx
+++ frontend/src/components/common/section/CircleImageSection.tsx
@@ -4,10 +4,10 @@ import React from "react";
 import CircleImage from "@/components/common/ui/CircleImage";
 import Lines from "@/components/common/ui/Lines";
 
-import hanroroMainImage from "../../../../public/photo/circle/hanroromain.webp";
+import hanroroMainImage2 from "../../../../public/photo/circle/hanroromain2.webp";
 
 export default function CircleImageSection() {
-  const circlePhotoList = [hanroroMainImage];
+  const circlePhotoList = [hanroroMainImage2];
 
   return (
     <section className=" relative   mb-20 mt-24 flex w-screen  snap-start  items-center justify-center">
