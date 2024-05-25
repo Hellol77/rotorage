0c0afe0833f8d4a2266a4d9d0d3d649541c2de0b
DongHyeonWon <dhe7700@naver.com>
2024-05-25 23:04:21 +0900

    chore: google analytics 변경

--- frontend/src/app/googleAnalytics.tsx
+++ frontend/src/app/googleAnalytics.tsx
@@ -5,12 +5,13 @@ import Script from "next/script";
 export default function GoogleAnalytics() {
   return (
     <>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ZE03PRDYFS"></Script>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-K3905R1WDZ"></Script>
       <Script id="ga-script">
-        {`window.dataLayer = window.dataLayer || [];
+        {` window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'G-ZE03PRDYFS');`}
+
+  gtag('config', 'G-K3905R1WDZ');`}
       </Script>
     </>
   );
