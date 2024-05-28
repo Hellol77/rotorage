ddc25e56d5a438fbe20c310460931a39d55f31ef
DongHyeonWon <dhe7700@naver.com>
2024-05-28 12:02:24 +0900

    feat: main page 스켈레톤 추가

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,24 +3,29 @@ import { queryKeys } from "@/apis/querykeys";
 import BoardPhotoCard from "@/components/common/card/boardPhotoCard/BoardPhotoCard";
 import BoardGridContainer from "@/components/common/container/BoardGridContainer";
 import CircleImageSection from "@/components/common/section/CircleImageSection";
+import PostSkeletonGrid from "@/components/common/skeleton/PostSkeletonGrid";
 import useGetRecentPosts from "@/hooks/queries/useGetRecentPosts";
 
 export default function Home() {
-  const { data } = useGetRecentPosts();
+  const { data, isPending } = useGetRecentPosts();
   return (
     <main className="z-1 relative  h-full w-screen  items-center overflow-y-scroll scrollbar-hide ">
       <CircleImageSection />
       <section className=" mx-auto mt-20 w-full max-w-[1280px] px-6 md:mt-40 md:px-20">
         <h1 className=" mb-8 font-poorStory text-3xl">Recent Posts</h1>
         <BoardGridContainer>
-          {data?.map((post) => (
-            <BoardPhotoCard
-              key={post.imageUrl}
-              post={post}
-              type="default"
-              queryKey={queryKeys.recentPosts}
-            />
-          ))}
+          {isPending ? (
+            <PostSkeletonGrid columns={4} />
+          ) : (
+            data?.map((post) => (
+              <BoardPhotoCard
+                key={post.imageUrl}
+                post={post}
+                type="default"
+                queryKey={queryKeys.recentPosts}
+              />
+            ))
+          )}
         </BoardGridContainer>
       </section>
     </main>
--- frontend/src/components/common/skeleton/Skeleton.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useMemo } from "react";
-
-export default function Skeleton() {
-  return <div className="">Skeleton</div>;
-}
-
-const SkeletonType = () => {};
